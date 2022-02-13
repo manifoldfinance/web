b502ab92914aa9c8f0c95e983c276312dff84f7d
sam bacha <sam@manifoldfinance.com>
2022-02-13 15:16:27 -0800

    feat(nextjs): faster builds

--- next.config.js
+++ next.config.js
@@ -5,6 +5,28 @@ const withOptimizedImages = require('next-optimized-images');
 
 const withTM = require('next-transpile-modules')(['@modulz/design-system']);
 
+// @ts-check
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  productionBrowserSourceMaps: false,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  experimental: { esmExternals: true },
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+    disable: process.env.NODE_ENV === 'development',
+  },
+  images: {
+    minimumCacheTTL: 1209600,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    domains: ['raw.githubusercontent.com'],
+  }
+}
+
 module.exports = withPlugins([withTM, withOptimizedImages, withVideos], {
   // Next.js config
   async redirects() {
@@ -16,4 +38,6 @@ module.exports = withPlugins([withTM, withOptimizedImages, withVideos], {
       },
     ];
   },
-});
+})(nextConfig);
+
+console.log('next.config.js', JSON.stringify(module.exports, null, 2))
