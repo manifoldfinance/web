fb0d0a789048e0146014b172afcf594112e2ec1a
sam bacha <sam@manifoldfinance.com>
2022-02-18 20:14:52 -0800

    fix(pages): update doc nav (#26)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,8 @@
+import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
+import { gtagUrl, renderSnippet } from '@lib/metrics';
+
 import React from 'react';
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 import { getCssText } from '@modulz/design-system';
-import { renderSnippet, gtagUrl } from '@lib/analytics';
 
 export default class Document extends NextDocument {
   render() {
