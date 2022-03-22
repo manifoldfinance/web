0a06c35389266766bc5174edfc1eac6e47534f97
sam bacha <sam@manifoldfinance.com>
2022-03-22 16:36:05 -0700

    fix(types): observer (#51)

---
+++ index.d.ts
@@ -0,0 +1,37 @@
+
+// Polyfill for ResizeObserver since type is missing from TS
+// https://github.com/que-etc/resize-observer-polyfill/blob/master/src/index.d.ts
+interface DOMRectReadOnly {
+    readonly x: number;
+    readonly y: number;
+    readonly width: number;
+    readonly height: number;
+    readonly top: number;
+    readonly right: number;
+    readonly bottom: number;
+    readonly left: number;
+}
+
+type ResizeObserverCallback = (entries: ResizeObserverEntry[], observer: ResizeObserver) => void;
+
+interface ResizeObserverEntry {
+    readonly target: Element;
+    readonly contentRect: DOMRectReadOnly;
+}
+
+interface ResizeObserver {
+    observe(target: Element): void;
+    unobserve(target: Element): void;
+    disconnect(): void;
+}
+
+declare const ResizeObserver: {
+    prototype: ResizeObserver;
+    new(callback: ResizeObserverCallback): ResizeObserver;
+};
+
+interface ResizeObserver {
+    observe(target: Element): void;
+    unobserve(target: Element): void;
+    disconnect(): void;
+}
