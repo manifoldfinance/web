a786ad757c3366e95e8d9d58f19b2fe52be103e4
sam bacha <sam@manifoldfinance.com>
2022-04-04 11:32:42 -0700

    fix(css): use stitches reet

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,17 +2,25 @@ import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
 import { gtagUrl, renderSnippet } from '@lib/metrics';
 
 import React from 'react';
-import { getCssText } from '@modulz/design-system';
+import { getCssText, reset } from '../stitches.config';
+
+/**
+ * Get the css and reset the internal css representation.
+ * This is very *IMPORTANT* to do as the server might handle multiple requests
+ * and we don't want to have the css accumulated from previous requests
+ */
+const getCssAndReset = () => {
+  const css = getCssText();
+  reset();
+  return css;
+};
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <style
-            id="stitches"
-            dangerouslySetInnerHTML={{ __html: getCssText() }}
-          />
+        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssAndReset() }} />
           <link
             rel="preload"
             href="/fonts/UntitledSansWeb-Regular.woff"
