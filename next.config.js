e3bf29f450b49faa7c001daa408dfc07e345df82
sam bacha <sam@manifoldfinance.com>
2022-05-13 04:17:55 -0700

    fix(build): mdx

--- next.config.js
+++ next.config.js
@@ -4,11 +4,22 @@ const withOptimizedImages = require('next-optimized-images');
 
 const withTM = require('next-transpile-modules')(['@modulz/design-system']);
 
-module.exports = withPlugins([withTM, withOptimizedImages], {
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+     providerImportSource: "@mdx-js/react",
+  },
+});
+
+module.exports = withPlugins([withMDX, withTM, withOptimizedImages], {
   future: {
     webpack5: true,
   },
   // Next.js config
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   poweredByHeader: false,
   async redirects() {
     return [
