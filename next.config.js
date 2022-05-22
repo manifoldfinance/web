f6be461d1ebb6692a6038fcdf434174b4dc8e5d8
sam bacha <sam@manifoldfinance.com>
2022-05-22 16:50:45 -0700

    feat(verify): expose env sha

--- next.config.js
+++ next.config.js
@@ -4,12 +4,27 @@ const withOptimizedImages = require('next-optimized-images');
 
 const withTM = require('next-transpile-modules')(['@modulz/design-system']);
 
+const {
+    NODE_ENV,
+    VERCEL_GITHUB_COMMIT_SHA,
+    VERCEL_GIT_COMMIT_SHA,
+    VERCEL_GIT_REPO_ID,
+    NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
+} = process.env
+
+
 module.exports = withPlugins([withTM, withOptimizedImages], {
   future: {
     webpack5: true,
   },
   // Next.js config
   poweredByHeader: false,
+  env: {
+    // Make the COMMIT_SHA available to the client so that Sentry events can be
+    // marked for the release they belong to. It may be undefined if running
+    // outside of Vercel
+    NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA: COMMIT_SHA,
+  },
   async redirects() {
     return [
       {
@@ -39,6 +54,7 @@ module.exports = withPlugins([withTM, withOptimizedImages], {
   },
 });
 
+// @see {@link https://vercel.com/docs/concepts/projects/environment-variables#system-environment-variables}
 // Don't delete this console log, useful to see the config in Vercel deployments
 console.log('process.env.VERCEL_GIT_COMMIT_SHA: ', process.env.VERCEL_GIT_COMMIT_SHA);
 console.log('process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA: ', process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA);
