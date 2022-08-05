2db7043439a822421bbc8c733ec5df1050ee4d02
sam bacha <sam@manifoldfinance.com>
2022-08-05 16:24:01 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,6 +32,11 @@ const globalStyles = globalCss({
 
   'html': {
     overflowX: 'hidden',
+    'position': 'fixed',
+    'top': 0,
+    'bottom': 0,
+    'left': 0,
+    'right': 0,
   },
 
   'svg': {
