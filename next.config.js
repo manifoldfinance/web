719c94c915cf8237d788abe27b581ada083fbab2
s bacha <sam@manifoldfinance.com>
2022-09-12 02:39:08 -0700

    fix(redirect): docs links
    fixes the vercel redirect links

--- next.config.js
+++ next.config.js
@@ -6,33 +6,39 @@ const withTM = require('next-transpile-modules')(['@modulz/design-system']);
 const date = new Date();
 const GIT_COMMIT_SHA_SHORT = typeof process.env.GIT_COMMIT_SHA === 'string' && process.env.GIT_COMMIT_SHA.substring(0, 8);
 
-module.exports = withPlugins([withTM, withOptimizedImages], {
+
+// @ts-check
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  productionBrowserSourceMaps: false,
+  poweredByHeader: false,
+  reactStrictMode: true,
   future: {
     webpack5: true,
   },
-  // Next.js config
-  poweredByHeader: false,
   env: {
     /**
      * @summary API Key Env Variables
      */
     // GITHUB_DISPATCH_TOKEN: process.env.GITHUB_DISPATCH_TOKEN,
-    SENTRY_DSN: process.env.SENTRY_DSN,
-    NEXT_PUBLIC_SENTRY_DSN: process.env.SENTRY_DSN, // Sentry DSN must be provided to the browser for error reporting to work there
+  //  SENTRY_DSN: process.env.SENTRY_DSN,
+  //  NEXT_PUBLIC_SENTRY_DSN: process.env.SENTRY_DSN, // Sentry DSN must be provided to the browser for error reporting to work there
     /**
     * @const VERCEL_
     * @see {@link https://vercel.com/docs/environment-variables#system-environment-variables}
     */
     VERCEL: process.env.VERCEL,
-    VERCEL_ENV: process.env.VERCEL_ENV,
-    VERCEL_URL: process.env.VERCEL_URL,
-    CI: process.env.CI,
+    NEXT_PUBLIC_VERCEL_ENV: process.env.VERCEL_ENV,
+    NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
+  //  CI: process.env.CI,
   },
   async redirects() {
     return [
       {
         source: '/docs',
-        destination: '/docs/introduction',
+        destination: 'https://kb.manifoldfinance.com',
         permanent: true,
       },
     ];
@@ -57,8 +63,10 @@ module.exports = withPlugins([withTM, withOptimizedImages], {
   },
 });
 
+module.exports = withPlugins(withTM(withOptimizedImages(nextConfig)));
+
 // Don't delete this console log, useful to see the config in Vercel deployments
+console.log('next.config.js', JSON.stringify(module.exports, null, 2));
+
 console.log('process.env.VERCEL_GIT_COMMIT_SHA: ', process.env.VERCEL_GIT_COMMIT_SHA);
 console.log('process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA: ', process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA);
-//console.log('process.env.GIT_COMMIT_SHA: ', process.env.GIT_COMMIT_SHA);
-console.log('next.config.js', JSON.stringify(module.exports, null, 2));
