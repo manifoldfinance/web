f0dc8cc3f2ce85c973a99634e3f7fcab6f78d95f
s bacha <sam@manifoldfinance.com>
2022-09-12 03:19:35 -0700

    refactor(nextjs): build

--- next.config.js
+++ next.config.js
@@ -1,72 +1,53 @@
-const readingTime = require('reading-time');
-const withPlugins = require('next-compose-plugins');
-const withOptimizedImages = require('next-optimized-images');
-
-const withTM = require('next-transpile-modules')(['@modulz/design-system']);
-const date = new Date();
-const GIT_COMMIT_SHA_SHORT = typeof process.env.GIT_COMMIT_SHA === 'string' && process.env.GIT_COMMIT_SHA.substring(0, 8);
-
-
 // @ts-check
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-  productionBrowserSourceMaps: false,
-  poweredByHeader: false,
-  reactStrictMode: true,
-  future: {
-    webpack5: true,
-  },
-  env: {
-    /**
-     * @summary API Key Env Variables
-     */
-    // GITHUB_DISPATCH_TOKEN: process.env.GITHUB_DISPATCH_TOKEN,
-  //  SENTRY_DSN: process.env.SENTRY_DSN,
-  //  NEXT_PUBLIC_SENTRY_DSN: process.env.SENTRY_DSN, // Sentry DSN must be provided to the browser for error reporting to work there
-    /**
-    * @const VERCEL_
-    * @see {@link https://vercel.com/docs/environment-variables#system-environment-variables}
-    */
-    VERCEL: process.env.VERCEL,
-    NEXT_PUBLIC_VERCEL_ENV: process.env.VERCEL_ENV,
-    NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
-  //  CI: process.env.CI,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/docs',
-        destination: 'https://kb.manifoldfinance.com',
-        permanent: true,
-      },
-    ];
-  },
-  async rewrites() {
-    return {
-      fallback: [
-        {
-          source: '/:path*',
-          destination: '/:path*/index.html',
-        },
-      ],
-    };
-  },
-  async headeres() {
-    return [
-      {
+ const readingTime = require('reading-time');
+ const withPlugins = require('next-compose-plugins');
+ const withOptimizedImages = require('next-optimized-images');
+ 
+ const withTM = require('next-transpile-modules')(['@modulz/design-system']);
+ 
+ module.exports = withPlugins([withTM, withOptimizedImages], {
+   future: {
+     webpack5: true,
+   },
+   // Next.js config
+   poweredByHeader: false,
+   reactStrictMode: true,
+   productionBrowserSourceMaps: false,
+   async redirects() {
+     return [
+       {
+         source: '/docs',
+         destination: 'https://kb.manifoldfinance.com',
+         permanent: true,
+       },
+     ];
+   },
+   async rewrites() {
+     return {
+       fallback: [
+         {
+           source: '/:path*',
+           destination: '/:path*/index.html',
+         },
+       ],
+     };
+   },
+   async headers() {
+     return [
+       {
         source: '/(.*)',
-        headers: [{ key: 'Web-Build', value: process.env.VERCEL_GIT_COMMIT_SHA }],
-      },
-    ];
-  },
-};
-
-module.exports = withPlugins(withTM(withOptimizedImages(nextConfig)));
-
-// Don't delete this console log, useful to see the config in Vercel deployments
-console.log('next.config.js', JSON.stringify(module.exports, null, 2));
-
-console.log('process.env.VERCEL_GIT_COMMIT_SHA: ', process.env.VERCEL_GIT_COMMIT_SHA);
-console.log('process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA: ', process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA);
+        headers: [{ key: 'Web-Build', value: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA }],
+       },
+     ];
+   },
+ });
+ 
+ 
+ // Don't delete this console log, useful to see the config in Vercel deployments
+ console.log('next.config.js', JSON.stringify(module.exports, null, 2));
+ 
+ console.log('process.env.VERCEL_GIT_COMMIT_SHA: ', process.env.VERCEL_GIT_COMMIT_SHA);
+ console.log('process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA: ', process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA);
